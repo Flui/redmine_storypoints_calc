1b493eba7a9bd6e60e359d4754df957256c19f6e
mb@evertices.com <mb@evertices.com>
2018-07-27 18:26:04 +0200

    refactoring

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Redmine Story Point Display
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.5
 // @description  Shows the sum of Story Points in columns which contains subtasks
 // @author       Marc Bohm
 // @match        http://tampermonkey.net/index.php?version=4.6&ext=dhdg&updated=true
@@ -13,68 +13,104 @@
 
     setTimeout(function() {
 
-        var tickets_in_ready_for_dev;
-        var storypoints
-        var backlog = [];
-        var tickets_in_bereit_fuer_entwicklung;
-        var backlog_subtasks = [];
-        var tmp_string;
-        var ticket_number;
-        var subtask_number;
-        var storypoints_second_row = 0;
+            var tickets_in_ready_for_dev;
+            var storypoints
+            var backlog = [];
+            var tickets_in_bereit_fuer_entwicklung;
+            var child_container;
+            var backlog_bereit_fuer_entwicklung = [];
+            var backlog_bearbeiten = [];
+            var backlog_codereview = [];
+            var tmp_string;
+            var ticket_number;
+            var subtask_number;
+            var storypoints_second_row = 0;
 
-        //backlog => build array of tickets including ticketnumber and storypoints. serves as source for calculations
+            //backlog => build array of tickets including ticketnumber and storypoints. serves as source for calculations
+            tickets_in_ready_for_dev = document.getElementsByClassName("backlog-column agile__list")[0].childNodes;
+            for (var i = 0; i < document.getElementsByClassName("backlog-column agile__list")[0].childNodes.length; i++) {
 
-        tickets_in_ready_for_dev = document.getElementsByClassName("backlog-column agile__list")[0].childNodes;
-        for (var i = 0; i < document.getElementsByClassName("backlog-column agile__list")[0].childNodes.length; i++) {
+                var childnode = document.getElementsByClassName("backlog-column agile__list")[0].childNodes[i]
+                var classname = childnode.className;
+                classname = classname.replace("agile__item item_", "")
+                storypoints = childnode.getElementsByClassName("agile__card__summable")[0].innerText;
+                backlog.push([classname, storypoints]);
+            }
 
-            var childnode = document.getElementsByClassName("backlog-column agile__list")[0].childNodes[i]
-            var classname = childnode.className;
-            classname = classname.replace("agile__item item_", "")
-            storypoints = childnode.getElementsByClassName("agile__card__summable")[0].innerText;
+            build_backlogs("Bereit für Entwicklung");
+            build_backlogs("Bearbeiten");
 
-            backlog.push([classname, storypoints]);
+            remove_duplicates(backlog_bearbeiten, backlog_bereit_fuer_entwicklung);
+            write_points(backlog, backlog_bereit_fuer_entwicklung);
+            write_points(backlog, backlog_bearbeiten);
 
-        }
+            calculate_backlog_points(backlog_bereit_fuer_entwicklung);
+            calculate_backlog_points(backlog_bearbeiten);
 
-        // build array of tickets of column "Bereit für Entwicklung" und "bearbeiten"
+            // build array of tickets of column "Bereit für Entwicklung" und "bearbeiten"
+            function build_backlogs(column_name) {
+                //build querySelector
+                var selector = "div[data-column-name='" + column_name + "']";
 
-
-        tickets_in_bereit_fuer_entwicklung = document.querySelectorAll("div[data-column-name='Bereit für Entwicklung']")[0].childNodes
-        for (var j = 0; j < tickets_in_bereit_fuer_entwicklung.length; j++) {
-            tmp_string = tickets_in_bereit_fuer_entwicklung[j].innerText;
-            ticket_number = tmp_string.substring(tmp_string.lastIndexOf('[') + 1, tmp_string.lastIndexOf(']')).replace("#", "");
-            subtask_number = tmp_string.substring(1, 5);
-
-            backlog_subtasks.push([ticket_number, 0, subtask_number]);
-        }
-
-        // check if ticketnumber of an item in array "backlog" is also in the title of an index of "Bereit für Entwicklung"
-        for (i = 0; i < backlog.length; i++) {
-            for (j = 0; j < backlog_subtasks.length; j++) {
-                if (backlog[i][0] == backlog_subtasks[j][0]) {
-                    backlog_subtasks[j][1] = backlog[i][1];
-
+                child_container = document.querySelectorAll(selector)[0].childNodes;
+                for (var j = 0; j < child_container.length; j++) {
+                    tmp_string = child_container[j].innerText;
+                    ticket_number = tmp_string.substring(tmp_string.lastIndexOf('[') + 1, tmp_string.lastIndexOf(']')).replace("#", "");
+                    subtask_number = tmp_string.substring(1, 5);
+                    if (column_name == "Bereit für Entwicklung") {
+                        backlog_bereit_fuer_entwicklung.push([ticket_number, 0, subtask_number]);
+                    } else {
+                        backlog_bearbeiten.push([ticket_number, 0, subtask_number]);
+                    }
                 }
             }
-        }
+            // check if ticketnumber of an item in array "backlog" is also in the title of an index of "Bereit für Entwicklung"
+            function write_points(original_backlog, column_backlog) {
+                for (i = 0; i < original_backlog.length; i++) {
+                    for (var j = 0; j < column_backlog.length; j++) {
+                        if (original_backlog[i][0] == column_backlog[j][0]) {
+                            column_backlog[j][1] = original_backlog[i][1];
+                        }
+                    }
+                }
+            };
 
-        //calcuate points in "Bereit für Entwicklung"
-        for(i=0;i<backlog_subtasks.length;i++){
-          if(backlog_subtasks[i][1] != "—"){
-              storypoints_second_row = storypoints_second_row + parseInt(backlog_subtasks[i][1]);
-          }
-        }
+            function remove_duplicates(original_backlog, column_backlog) {
+              for (i = 0; i < original_backlog.length; i++) {
 
-        //do the same for all other columns
+                  for (var j = 0; j < column_backlog.length; j++) {
 
+                      if (original_backlog[i][0] == column_backlog[j][0]) {
+                        console.log("Cut out " + original_backlog[i][0]);
+                         original_backlog.splice(i,1);
+                         --i;
+                      }
+                    
+                  }
 
-        console.log(storypoints_second_row + " Storypoints sind bereit für die Entwicklung");
-        var endresult = document.querySelectorAll("#scrum-sprint-container > div.agile > div > div.easy-col.col1.agile__main-col.agile__col > div > div > div > div.agile__col.sprint-col.col0 > div.agile__col__title.sticky > div > span.agile__col__title__details")
-        var html = endresult[0].innerText;
-        endresult[0].innerText += (" " + storypoints_second_row);
+              }
+            }
+
+            //calcuate points in "Bereit für Entwicklung"
+            function calculate_backlog_points(backlog_array) {
+                for (i = 0; i < backlog_array.length; i++) {
+                    if (backlog_array[i][1] != "—") {
+                        storypoints_second_row = storypoints_second_row + parseInt(backlog_array[i][1]);
+                    }
+                }
+                //write to column header
+                if (backlog_array == backlog_bereit_fuer_entwicklung) {
+                    var endresult = document.querySelectorAll("#scrum-sprint-container > div.agile > div > div.easy-col.col1.agile__main-col.agile__col > div > div > div > div.agile__col.sprint-col.col0 > div.agile__col__title.sticky > div > span.agile__col__title__details")
+                    var html = endresult[0].innerText;
+                    endresult[0].innerText += (" " + storypoints_second_row);
+                } else {
+                    endresult = document.querySelectorAll("#scrum-sprint-container > div.agile > div > div.easy-col.col1.agile__main-col.agile__col > div > div > div > div.agile__col.sprint-col.col1 > div.agile__col__title.sticky > div > span.agile__col__title__details")
+                    html = endresult[0].innerText;
+                    endresult[0].innerText += (" " + storypoints_second_row);
+                }
+            }
         },
-               5500);
+        5500);
 
 
 })();
